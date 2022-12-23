065c674210b6767f1975a1735091a5456e5822ff
Ada Barbulescu <adabarbu2000@yahoo.com>
2022-12-23 13:58:44 +0200

    day5

---
+++ day 5/main.py
@@ -0,0 +1,89 @@
+import random
+
+print("Welcome to the PyPassword Generator")
+no_letters = 0
+no_symbols = 0
+no_numbers = 0
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+while True:
+    try:
+        no_letters = int(input("How many letters would you like in your password?\n"))
+        no_symbols = int(input("How many symbols would you like in your password?\n"))
+        no_numbers = int(input("How many numbers would you like in your password?\n"))
+    except ValueError:
+        print("Please enter a valid integer.")
+        continue
+    break
+
+password = []
+# Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+for i in range(no_letters):
+    password.append(random.choice(letters))
+for i in range(no_symbols):
+    password.append(random.choice(symbols))
+for i in range(no_numbers):
+    password.append(random.choice(numbers))
+# print(f'Here is your password: {"".join(password)}')
+
+# Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+random.shuffle(password)
+print(f'Here is your password: {"".join(password)}')
