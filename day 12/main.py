7ae379bc81ea5649457167e41a44dfb18d2532cf
Ada Barbulescu <adabarbu2000@yahoo.com>
2022-12-25 17:47:21 +0200

    day 12

--- day 12/main.py
+++ day 12/main.py
@@ -1,41 +1,60 @@
-from art import logo
 import random
-
-print(logo)
-
-
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-to_guess = random.randint(1, 100)
+from art import logo
 
 
-def guess(no_lives):
-    while no_lives >= 0:
-        if no_lives == 0:
-            print(f"You've run out of guesses. The answer was {to_guess}.")
-            break
+def welcome_message():
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+
+def choose_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty == "easy":
+        return 10
+    else:
+        return 5
+
+
+def get_guess():
+    while True:
         try:
-            player_guess = int(input("Make a guess: "))
+            guess = int(input("Make a guess: "))
+            return guess
         except ValueError:
             print("Invalid input. Please enter a number.")
-            continue
-        if player_guess < to_guess:
-            print("Too low.")
-            no_lives = no_lives - 1
-            guess(no_lives)
-        if player_guess > to_guess:
-            print("Too high")
-            no_lives = no_lives - 1
-            guess(no_lives)
-        if player_guess == to_guess:
-            print(f"You got it. The answer was {to_guess}!")
-            break
 
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-if difficulty == "easy":
-    no_lives = 10
-else:
-    no_lives = 5
+def evaluate_guess(guess, to_guess):
+    if guess < to_guess:
+        print("Too low.")
+        return False
+    if guess > to_guess:
+        print("Too high")
+        return False
+    if guess == to_guess:
+        print(f"You got it. The answer was {to_guess}!")
+        return True
 
-guess(no_lives)
+
+def game_over(no_lives):
+    print(f"You've run out of guesses. The answer was {to_guess}.")
+
+
+def play_game(no_lives):
+    to_guess = random.randint(1, 100)
+    while no_lives >= 0:
+        guess = get_guess()
+        if evaluate_guess(guess, to_guess):
+            return
+        no_lives = no_lives - 1
+    game_over(no_lives)
+
+
+def main():
+    welcome_message()
+    no_lives = choose_difficulty()
+    play_game(no_lives)
+
+
+main()
