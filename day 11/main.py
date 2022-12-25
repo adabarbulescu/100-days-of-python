4e8c8ba0d43ceddf9fe112d070937ca766cf62c6
Ada Barbulescu <adabarbu2000@yahoo.com>
2022-12-25 16:07:50 +0200

    day 11

--- day 11/main.py
+++ day 11/main.py
@@ -25,8 +25,10 @@ def compare(player_score, computer_score):
     if player_score == computer_score:
         return "Draw."
     elif player_score == 0:
+        player_score = 21
         return "Win with a Blackjack."
     elif computer_score == 0:
+        computer_score = 21
         return "You lose. Opponent has Blackjack."
     elif player_score > 21:
         return "You went over. You lose"
@@ -38,12 +40,8 @@ def compare(player_score, computer_score):
         return "You lose."
 
 
-def check_if_over(player, computer):
-    if (
-        (calculate_score(player)) == 0
-        or calculate_score(computer) == 0
-        or calculate_score(player) > 21
-    ):
+def check_if_over(player_score, computer_score):
+    if player_score == 0 or computer_score == 0 or player_score > 21:
         return 1
 
 
@@ -52,39 +50,36 @@ def game():
     computer = []
     print(logo)
     continuation = "y"
+    for i in range(2):
+        player.append(dealcards())
+        computer.append(dealcards())
 
-    # de aici incepe while ul
     while continuation == "y":
-        continuation = input(
-            "Do you want to play a game of Blackjack? Type 'y' or 'n': "
-        )
-        if continuation == "n":
+        player_score = calculate_score(player)
+        computer_score = calculate_score(computer)
+
+        print(f"Your cards: {player}, current score: {player_score}")
+        print(f"Computer's first card: {computer[0]}")
+        if check_if_over(player_score, computer_score) == 1:
+            continuation = "n"
             break
+
         else:
-            for i in range(2):
+            # the game continues
+            dealing = input("Type 'y' to get another card, type 'n' to pass: ")
+            if dealing == "y":
                 player.append(dealcards())
-                computer.append(dealcards())
-            print(f"Your cards: {player}, current score: {calculate_score(player)}")
-            print(f"Computer's first card: {computer[0]}")
-            if check_if_over(player, computer) == 1:
-                # the game ends
-                # print your cards
-                # deal computer more cards
-                # compare and choose a winner
-                break
-
             else:
-                # the game continues
-                continuation = input("Type 'y' to get another card, type 'n' to pass: ")
-                if continuation == "n":
-                    break
-                else:
-                    player.append(dealcards())
-                    computer.append(dealcards())
+                continuation = "n"
+    while computer_score != 0 and computer_score < 17:
+        computer.append(dealcards())
+        computer_score = calculate_score(computer)
 
-                    if check_if_over(player, computer) == 1:
-                        break
+    print(f"Your final hand: {player}, final score: {player_score}")
+    print(f"Computer's final hand: {computer}, final score: {computer_score}")
+    print(compare(player_score, computer_score))
 
 
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    clear()
     game()
